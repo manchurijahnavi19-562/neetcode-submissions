d600fe438dd304b8e591121e29239b193d9fc27a
Jahnavi manchuri <manchurijahnavi19@gmail.com>
2026-08-05 12:46:36 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit=0;
+        int minprice=prices[0];
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i] < minprice)
+            {
+                minprice=prices[i];
+            }
+            else
+            {
+                int profit=prices[i]-minprice;
+                if(profit>maxprofit)
+                {
+                    maxprofit=profit;
+                }
+
+            }
+
+        }
+        return maxprofit;
+        
+    }
+}
